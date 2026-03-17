d4d4c3fe27e7da3f6f6d668ff20edd7271704356
Yasmin Kimura <161663934+kimurinhakiki@users.noreply.github.com>
2026-03-17 09:50:48 -0300

    Delete rpa_rov_prisma directory

--- rpa_rov_prisma/constants.py
+++
@@ -1,9 +0,0 @@
-# API Departamental
-API_BASE_URL = 'http://10.124.100.160:8085/api'
-API_GET_TOKEN_ENDPOINT = 'Authentication/Login'
-API_GET_OCORRENCIAS_ENDPOINT = 'Ocorrencias/PegarOcorrenciasParaAtualizar'
-API_UPDATE_OCORRENCIA_ENDPOINT = 'Ocorrencias/AtualizarOcorrencia'
-APPLICATION_NAME = 'RPA ROV/PRISMA'
-APPLICATION_VERSION = '2.1.0'
-DB_PATH = 'logs.db'
-HOMOLOGACAO_PORT = 8086
